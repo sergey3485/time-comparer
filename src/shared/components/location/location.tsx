b68f6c366ac920ad372cf1524d2f2b7ba5e20ce8
sergey <33asasergey33@gmail.com>
2023-07-26 00:57:34 +0300

    fix: locations card styles

--- src/shared/components/location/location.tsx
+++ src/shared/components/location/location.tsx
@@ -20,7 +20,7 @@ import {
   getMils,
   getTimezoneDifference,
   getTimezoneInHours,
-  isDayEqual,
+  isDayEqualInTwoLocations,
 } from '@/shared/lib/time';
 
 import { isTwoLocationEqual } from '@/shared/lib/location/is-two-location-equal';
@@ -67,6 +67,10 @@ export const Location = (props: LocationProps): JSX.Element => {
 
   const timezoneDifference = getTimezoneDifference(location.timezone, selectedLoc?.timezone as string);
 
+  const isSelected = isTwoLocationEqual(selectedLoc as City, location);
+
+  const isDayEqual = isDayEqualInTwoLocations(selectedLoc as City, location, time);
+
   return (
     <Box
       display="flex"
@@ -76,7 +80,9 @@ export const Location = (props: LocationProps): JSX.Element => {
       width="100%"
       height="158px"
       onPointerDown={() => selectLoc(location)}
-      backgroundColor={isTwoLocationEqual(selectedLoc as City, location) ? 'blackAlpha.300' : 'white'}
+      border="2px solid"
+      background="white"
+      borderColor={isSelected ? 'blue.500' : 'white'}
       role="listitem"
       position="relative"
     >
@@ -116,12 +122,12 @@ export const Location = (props: LocationProps): JSX.Element => {
           <Text fontSize="sm" fontWeight={500} color="gray.500" textAlign="start">GMT{timeZone > 0 ? `+${timeZone}` : timeZone}</Text>
         </Box>
 
-        <Text fontSize="sm" fontWeight={500} color={isDayEqual(selectedLoc as City, location, time) ? 'gray.500' : 'red'} textAlign="start">{day}</Text>
+        <Text fontSize="sm" fontWeight={500} color={isDayEqual ? 'gray.500' : 'red'} textAlign="start">{day}</Text>
 
         <Box mt="auto">
           <Stack direction="row" alignItems="center">
             <Text style={{ fontVariantNumeric: 'tabular-nums slashed-zero' }} letterSpacing="-1.5px" fontSize="3xl" fontWeight={500} color="blackAlpha.900" textAlign="start">{currentTime}</Text>
-            {!isTwoLocationEqual(selectedLoc as City, location) && <Tag size="md" variant="subtle" colorScheme={timezoneDifference >= 0 ? 'green' : 'red'} textAlign="start">{timezoneDifference >= 0 ? `+${timezoneDifference}` : `${timezoneDifference}`} H</Tag>}
+            {!isSelected && <Tag size="md" variant="subtle" colorScheme={timezoneDifference >= 0 ? 'green' : 'red'} textAlign="start">{timezoneDifference >= 0 ? `+${timezoneDifference}` : `${timezoneDifference}`} H</Tag>}
           </Stack>
           <TimeSlider timeValue={milsValue} changeLocation={() => selectLoc(location)} />
         </Box>
