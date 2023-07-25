297e9d3d8644f3b1a34d561f0593a236a6c66193
sergey <33asasergey33@gmail.com>
2023-07-26 00:52:02 +0300

    fix: rename function

--- src/shared/lib/time/is-day-equal-in-two-locations.ts
+++ src/shared/lib/time/is-day-equal-in-two-locations.ts
@@ -1,7 +1,7 @@
 import formatInTimeZone from 'date-fns-tz/formatInTimeZone';
 import { City } from 'worldcities/lib/city';
 
-export const isDayEqual = (selected: City, current: City, time: Date): boolean => {
+export const isDayEqualInTwoLocations = (selected: City, current: City, time: Date): boolean => {
   const selectedLocationDay = formatInTimeZone(time, selected.timezone, 'dd MMMM');
   const currentLocationDay = formatInTimeZone(time, current.timezone, 'dd MMMM');
 
