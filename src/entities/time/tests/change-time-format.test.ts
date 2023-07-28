aacff09bd70d43fc641e229ed1eb11118492a96a
sergey <33asasergey33@gmail.com>
2023-07-28 21:45:42 +0300

    fix: delete useless imports

--- src/entities/time/tests/change-time-format.test.ts
+++ src/entities/time/tests/change-time-format.test.ts
@@ -2,8 +2,7 @@ import { fork, allSettled } from 'effector';
 import { describe, it, expect } from 'vitest';
 
 import { format24hours, format12hours } from '@/shared/lib/time-format';
-import { $timeFormat } from '@/entities/time';
-import { changeTimeFormat } from '@/features/change-time-format';
+import { $timeFormat, changeTimeFormat } from '@/entities/time';
 
 describe('change time format', () => {
   it('должен сменить формат времени', async () => {
